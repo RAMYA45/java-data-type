fea8292bdfed81a91d99f06c5279ce107f5d88a6
RAMYA45 <43058763+RAMYA45@users.noreply.github.com>
2018-09-12 15:48:21 +0530

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,31 @@
+class Main {
+  public static void main(String[] args) {
+			int DayOfTheWeek;
+      DayOfTheWeek=1;
+      switch(DayOfTheWeek)
+      {
+        case 0:
+        System.out.println("Free Personal Pizza on Purchse of medium pizza");
+        break;
+        case 1:
+        System.out.println("Rs.101 oFF on a bill of Rs.400 and above");
+        break;
+        case 2:
+        System.out.println("Free Plain Garlic Bread on another medium pizza");
+        break;
+        case 3:
+        System.out.println("Buy 1 Get 1 Free on any medium pizza");
+        break;
+        case 4:
+        System.out.println("Buy any medium pizza and get 50% off on another medium pizza");
+        break;
+        case 5:
+        System.out.println("Tastiest pizza 20% off on bill of Rs.399 and above");
+        break;
+        default:
+        System.out.println("Get Together Offer Free Garlic Bread and Dip on a min bill of Rs.350");
+      }
+
+	}
+}              
+  
