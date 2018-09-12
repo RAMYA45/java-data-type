74c4261beb1f915370e841a7c695e491b7636790
RAMYA45 <43058763+RAMYA45@users.noreply.github.com>
2018-09-12 15:48:58 +0530

    Add files via upload

---
+++ If (1).java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int DaysInTheatre;
+    DaysInTheatre=43;
+    if (DaysInTheatre>=50)
+    System.out.println("Box Office Hit");
+    if(DaysInTheatre<50)
+    System.out.println("Flop");
+  }
+}
